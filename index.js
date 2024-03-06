265ca44f6e7af71a6385fb3f134b9889e7d5f4ed
Joan Fernandez <hincapiefernandezjoan123@gmail.com>
2024-03-05 19:24:19 -0500

    Se creó un comando para obtener la longitu de un array

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-let array = [];
-
-array.push(["nombre", "Joan"]);
-array.push(['apellido', "Hincapie"]);
-array.push(['edad', 21]);
-
-array.forEach(function(datos) {
-  console.log(datos);
-});
+let ropa = ['zapatos','jean','camisa','medias','boxer']
+console.log(ropa.length)
