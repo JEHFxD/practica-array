b8f80beea22836add5f522c32cd70d51d5018206
Joan Fernandez <hincapiefernandezjoan123@gmail.com>
2024-03-05 19:42:17 -0500

    se creó un array con el metodo pop

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function generarArray(altura, minimo, maximo) {
-    let array = [];
-    for (let i = 0; i < altura; i++) {
-        let numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
-        array.push(numeroAleatorio);
-    }
-    return array;
-}
-
-let miArray = generarArray(30, 5, 210);
-console.log(miArray);
+let juegos = ["halo","wow","gow"]
+let ultimojuego = juegos.pop()
+console.log(juegos)
+console.log(ultimojuego)
